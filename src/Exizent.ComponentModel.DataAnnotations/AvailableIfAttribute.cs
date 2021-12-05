8dc8b0fa7409278b8f82b1b766c6b2a393f17885
Kevin Smith <kev_bite@msn.com>
2021-12-05 22:58:14 +0000

    Adding AvailableIf Attribute and tests

---
+++ src/Exizent.ComponentModel.DataAnnotations/AvailableIfAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+public class AvailableIfAttribute : ValidationAttribute
+{
+    public string DependentProperty { get; }
+    public object? DependantPropertyRequiredValue { get; }
+
+    public AvailableIfAttribute(string dependentProperty, object? dependantPropertyRequiredValue)
+    {
+        DependentProperty = dependentProperty;
+        DependantPropertyRequiredValue = dependantPropertyRequiredValue;
+    }
+
+    private bool IsValid(object? value, object? dependentPropertyValue)
+    {
+        return value is null
+               || Equals(dependentPropertyValue, DependantPropertyRequiredValue);
+    }
+    
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        var result = ValidationResult.Success!;
+        var dependentProperty = validationContext.ObjectType.GetRuntimeProperty(DependentProperty);
+        var dependentPropertyValue = dependentProperty.GetValue(validationContext.ObjectInstance, null);
+
+        if (!IsValid(value, dependentPropertyValue))
+        {
+            var memberNames = validationContext.MemberName != null
+                ? new[] { validationContext.MemberName }
+                : null;
+            
+            result = new ValidationResult($"{dependentProperty.Name} must be set to {FormatDependantPropertyRequiredValue()} to assign {value} to {validationContext.MemberName}", memberNames);
+        }
+
+        return result;
+    }
+
+    private string FormatDependantPropertyRequiredValue()
+        => DependantPropertyRequiredValue is null ? "null" : DependantPropertyRequiredValue.ToString();
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/AvailableIfAttributeTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class AvailableIfAttributeTests
+{
+    class BooleanTestModel
+    {
+        public bool BoolProp { get; set; }
+        [AvailableIf(nameof(BoolProp), true)]
+        public string? AvailableIfBoolProp { get; set; }
+    }
+    
+    class NullBooleanTestModel
+    {
+        public bool? BoolProp { get; set; }
+        [AvailableIf(nameof(BoolProp), null)]
+        public string? AvailableIfBoolProp { get; set; }
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void ShouldBeValidForAnyDependantPropertyRequiredValueWhenAvailableIfPropertyValueIsNull(bool dependantPropertyRequiredValue)
+    {
+        var model = new BooleanTestModel { BoolProp = dependantPropertyRequiredValue, AvailableIfBoolProp = null };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+        
+    [Fact]
+    public void ShouldBeValidWhenDependantPropertyRequiredValueMatches()
+    {
+        var model = new BooleanTestModel { BoolProp = true, AvailableIfBoolProp = "test" };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+    
+    [Fact]
+    public void ShouldBeInvalidWhenDependantPropertyRequiredValueDoesNotMatch()
+    {
+        var model = new BooleanTestModel { BoolProp = false, AvailableIfBoolProp = "test" };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"{nameof(BooleanTestModel.BoolProp)} must be set to {true} to assign {model.AvailableIfBoolProp} to {nameof(BooleanTestModel.AvailableIfBoolProp)}");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(BooleanTestModel.AvailableIfBoolProp));
+    }
+
+    [Fact]
+    public void ShouldBeValidWhenDependantPropertyRequiredValueIsNull()
+    {
+        var model = new NullBooleanTestModel { BoolProp = null, AvailableIfBoolProp = "test" };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+    
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void ShouldBeInvalidWhenDependantPropertyRequiredValueIsNotNull(bool boolProp)
+    {
+        var model = new NullBooleanTestModel { BoolProp = boolProp, AvailableIfBoolProp = "test" };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"{nameof(BooleanTestModel.BoolProp)} must be set to null to assign {model.AvailableIfBoolProp} to {nameof(BooleanTestModel.AvailableIfBoolProp)}");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(BooleanTestModel.AvailableIfBoolProp));
+    }
+}
