924d1a19d9c03edbc15ac6571dc072b1e8c9f176
Kevin Smith <kev_bite@msn.com>
2021-12-07 09:55:27 +0000

    Adding CurrencyAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/CurrencyAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class CurrencyAttribute : ValidationAttribute
+{
+    private static readonly CultureInfo EnglishUnitedKingdomCulture = CultureInfo.GetCultureInfo("en-GB");
+
+    public CurrencyAttribute()
+        : base("The field {0} is not a valid currency value.")
+    {
+        
+    }
+    public override bool IsValid(object? value)
+    {
+        if (value == null)
+        {
+            return true;
+        }
+
+        return IsLessThan(EnglishUnitedKingdomCulture.NumberFormat.CurrencyDecimalDigits, (decimal)value);
+    }
+    
+    private static bool IsLessThan(int decimalPlaces, decimal dec)
+    {
+        var value = dec * (int)Math.Pow(10, decimalPlaces);
+        
+        return value == Math.Floor(value);
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/CurrencyAttributeTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class CurrencyAttributeTests
+{
+    class TestModel
+    {
+        [CurrencyAttribute]
+        public decimal? Value { get; set; }
+    }
+
+    [Theory]
+    [InlineData(0.01)]
+    [InlineData(0.02)]
+    [InlineData(9.99)]
+    [InlineData(9999.00)]
+    [InlineData(9999)]
+    public void ShouldBeValidForValidEnglishUnitedKingdomCurrencyValue(decimal value)
+    {
+        var model = new TestModel { Value = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+    
+    [Theory]
+    [InlineData(0.001)]
+    [InlineData(0.002)]
+    public void ShouldBeInvalidForInvalidEnglishUnitedKingdomCurrencyValue(decimal value)
+    {
+        var model = new TestModel { Value = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should()
+            .Be($"The field {nameof(TestModel.Value)} is not a valid currency value.");
+        results[0].MemberNames.Should().BeEquivalentTo(nameof(TestModel.Value));
+    }
+    
+}
