b8b554d578c015da0b3e8c9041d6967296f598fc
Kevin Smith <kev_bite@msn.com>
2021-12-08 16:55:04 +0000

    Adding PostcodeAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/PostcodeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class PostcodeAttribute : DataTypeRegularExpressionAttribute
+{
+    const string regex =
+        @"^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) ?[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
+
+    public PostcodeAttribute()
+        : base(DataType.PostalCode, regex)
+    {
+        this.ErrorMessage = "The field {0} must be a valid postcode.";
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/PostcodeAttributeTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class PostcodeAttributeTests
+{
+    class TestModel
+    {
+        [Postcode]
+        public string? Postcode { get; set; }
+    }
+
+    [Theory]
+    [InlineData("PA1 0AF")]
+    [InlineData("AB10 0BA")]
+    public void ShouldBeValidForAValidPostcode(string value)
+    {
+        var model = new TestModel { Postcode = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("PA1  0AF")]
+    [InlineData("000000")]
+    [InlineData("PA PAa")]
+    [InlineData("PPPPPPP")]
+    public void ShouldBeInvalidForAInvalidPostcode(string value)
+    {
+        var model = new TestModel { Postcode = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"The field {nameof(TestModel.Postcode)} must be a valid postcode.");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestModel.Postcode));
+    }
+}
