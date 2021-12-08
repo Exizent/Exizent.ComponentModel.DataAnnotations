ad3b51a25f2fc1dd9c7b8c38e8dd1bf4f10b3a29
Kevin Smith <kev_bite@msn.com>
2021-12-08 19:29:34 +0000

    Adding RequiredIfAttribute.

---
+++ src/Exizent.ComponentModel.DataAnnotations/RequiredIfAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class RequiredIfAttribute : ValidationAttribute
+{
+    private readonly RequiredAttribute _innerAttribute;
+    private readonly object _requiredValue;
+    private readonly string _dependentProperty;
+
+    public RequiredIfAttribute(string dependentProperty, object requiredValue)
+    {
+        _innerAttribute = new RequiredAttribute();
+        _dependentProperty = dependentProperty;
+        _requiredValue = requiredValue;
+    }
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        var dependentProperty = validationContext.ObjectType.GetRuntimeProperty(_dependentProperty);
+        var dependentPropertyValue = dependentProperty.GetValue(validationContext.ObjectInstance, null);
+        
+        return _requiredValue.Equals(dependentPropertyValue)
+            ? ValidateInnerAttribute(value, validationContext)
+            : ValidationResult.Success!;
+    }
+
+    private ValidationResult ValidateInnerAttribute(object? value, ValidationContext validationContext)
+    {
+        if (!_innerAttribute.IsValid(value))
+        {
+            return new ValidationResult($"The field {validationContext.DisplayName} is required if {_dependentProperty} is {_requiredValue}.",
+                new[] { validationContext.MemberName! });
+        }
+
+        return ValidationResult.Success!;
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/RequiredIfAttributeTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class RequiredIfAttributeTests
+{
+    class TestClass
+    {
+        public TestEnum Dependency { get; set; }
+
+        [RequiredIf(nameof(Dependency), TestEnum.TestOne)]
+        public string? Value { get; set; }
+    }
+
+    enum TestEnum
+    {
+        Default,
+        TestOne,
+        TestTwo
+    }
+    
+    
+    [Fact]
+    public void ShouldBeValidWhenDependencyDoesNotMatchRequiredValue()
+    {
+        var model = new TestClass
+        {
+            Dependency = TestEnum.TestTwo
+        };
+
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+          
+    }
+
+
+    [Fact]
+    public void ValidationFailsIfDependentEnumValueMatchesRequiredIfValue()
+    {
+        var model = new TestClass
+        {
+            Dependency = TestEnum.TestOne
+        };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"The field {nameof(TestClass.Value)} is required if {nameof(TestClass.Dependency)} is {TestEnum.TestOne}.");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestClass.Value));
+    }
+
+}
