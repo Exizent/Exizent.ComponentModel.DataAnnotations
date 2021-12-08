59b948d1301096d9ba6c5af3000e1e319a0b4d92
Kevin Smith <kev_bite@msn.com>
2021-12-08 14:29:56 +0000

    Adding in BankAccountNumberAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/BankAccountNumberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class BankAccountNumberAttribute : RegularExpressionAttribute
+{
+    public BankAccountNumberAttribute()
+        : base(@"^[0-9]{6,10}$")
+    {
+        ErrorMessage = "The field {0} must be a valid bank account number.";
+    }
+}
