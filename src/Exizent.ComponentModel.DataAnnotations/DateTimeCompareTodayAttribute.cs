5d86e05a6c2e1ddec32b4c0bb6eb490480365f74
Kevin Smith <kev_bite@msn.com>
2021-12-08 22:39:12 +0000

    Refactoring date attributes to use ErrorMessageString so that they're overridable by custom text.

--- src/Exizent.ComponentModel.DataAnnotations/DateTimeCompareTodayAttribute.cs
+++ src/Exizent.ComponentModel.DataAnnotations/DateTimeCompareTodayAttribute.cs
@@ -3,14 +3,10 @@ namespace Exizent.ComponentModel.DataAnnotations;
 public class DateTimeCompareTodayAttribute : DateTimeCompareBaseAttribute
 {
     public DateTimeCompareTodayAttribute(EqualityCondition equalityCondition)
-        : base(equalityCondition)
+        : base(equalityCondition, "The field {0} must be {1} today.")
     {
-        
     }
-    
+
     protected override DateTime? GetOtherDateTimeValue(ValidationContext validationContext)
         => DateTime.Today;
-    
-    protected override string FormatErrorMessage(ValidationContext validationContext) =>
-        $"The field {validationContext.DisplayName} must be {FormatEqualityCondition()} today.";
 }
