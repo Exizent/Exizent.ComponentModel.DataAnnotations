b23df47c55778f0861740aa72dc1edada2270d2d
Kevin Smith <kev_bite@msn.com>
2021-12-08 16:21:14 +0000

    Adding PhoneNumberAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/PhoneNumberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class PhoneNumberAttribute : RegularExpressionAttribute
+{
+    public PhoneNumberAttribute()
+        : base(@"^[0-9]{5,12}$")
+    {
+        ErrorMessage = "The field {0} must be a valid phone number.";
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/PhoneNumberAttributeTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class PhoneNumberAttributeTests
+{
+    class TestModel
+    {
+        [PhoneNumber]
+        public string? PhoneNumber { get; set; }
+    }
+
+    [Theory]
+    [InlineData("00000")]
+    [InlineData("000000")]
+    [InlineData("0000000")]
+    [InlineData("00000000")]
+    [InlineData("000000000")]
+    [InlineData("0000000000")]
+    [InlineData("00000000000")]
+    [InlineData("000000000000")]
+    [InlineData("01123456798")]
+    public void ShouldBeValidForAValidPhoneNumber(string value)
+    {
+        var model = new TestModel { PhoneNumber = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("0")]
+    [InlineData("00")]
+    [InlineData("000")]
+    [InlineData("0000")]
+    [InlineData("0000000000000")]
+    [InlineData("(01112)222222")]
+    [InlineData("+442222222222")]
+    public void ShouldBeInvalidForAInvalidPhoneNumber(string value)
+    {
+        var model = new TestModel { PhoneNumber = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"The field {nameof(TestModel.PhoneNumber)} must be a valid phone number.");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestModel.PhoneNumber));
+    }
+}
