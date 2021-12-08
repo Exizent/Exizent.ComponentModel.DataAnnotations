6ac7b5e5b955aac9d5f58938673c84e6d41af803
Kevin Smith <kev_bite@msn.com>
2021-12-08 16:31:14 +0000

    Adding EmailAddressAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/EmailAddressAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class EmailAddressAttribute : RegularExpressionAttribute
+{
+    public EmailAddressAttribute()
+        : base(@"[^\s@]+@[^\s\.]+\..+")
+    {
+        ErrorMessage = "The field {0} must be a valid email address.";
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/EmailAddressAttributeTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class EmailAddressAttributeTests
+{
+    class TestModel
+    {
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+    }
+
+    [Theory]
+    [InlineData("ben.davidson@exizent.com")]
+    [InlineData("ghghjdgfhjds133243243212.32323232@subdomain.website.co.uk")]
+    public void ShouldBeValidForAValidEmailAddress(string value)
+    {
+        var model = new TestModel { EmailAddress = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeTrue();
+        results.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("ben@")]
+    [InlineData("exizent.com")]
+    [InlineData("ben")]
+    [InlineData(" @ . com")]
+    public void ShouldBeInvalidForAInvalidEmailAddress(string value)
+    {
+        var model = new TestModel { EmailAddress = value };
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+            
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        using var _ = new AssertionScope();
+        isValid.Should().BeFalse();
+        results[0].ErrorMessage.Should().Be($"The field {nameof(TestModel.EmailAddress)} must be a valid email address.");
+        results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestModel.EmailAddress));
+    }
+}
