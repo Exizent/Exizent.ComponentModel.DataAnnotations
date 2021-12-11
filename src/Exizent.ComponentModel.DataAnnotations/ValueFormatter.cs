4e5ce543da4d506e3a575988c071d6b3eccb453b
Kevin Smith <kev_bite@msn.com>
2021-12-11 21:20:13 +0000

    Adding AvailableValuesIfContainsAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/ValueFormatter.cs
@@ -0,0 +1,20 @@
+﻿namespace Exizent.ComponentModel.DataAnnotations;
+
+internal static class ValueFormatter
+{
+    public static string FormatOrValues(object[] values)
+        => FormatValues(values, "or");
+    
+    public static string FormatAndValues(object[] values)
+        => FormatValues(values, "or");
+
+    private static string FormatValues(object[] values, string joiningWord)
+    {
+        if (values is { Length: 1 })
+            return values[0].ToString() ?? string.Empty;
+
+        var formatted = string.Join(", ", values[..^1]);
+
+        return formatted + " " + joiningWord + " " + values[^1];
+    }
+}
