62ef56f7d794950b53c10e4d11bbdc8249f46e08
Sakis Koltsidas <sakis.koltsidas@exizent.com>
2022-01-14 14:26:08 +0000

    Add StringValueLength attribute for minimum and maximum size validation on values for KeyValue<,> pair types

---
+++ src/Exizent.ComponentModel.DataAnnotations/StringValueLengthAttribute.cs
@@ -0,0 +1,44 @@
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class StringValueLengthAttribute : StringLengthAttribute
+{
+    public StringValueLengthAttribute(int maximumLength) : base(maximumLength)
+    {
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        
+        // Validates and throws exception based on the range setup in the constructor
+        IsValid(null);
+        
+        var result = ValidationResult.Success;
+
+        if(value == null)
+        {
+            return result;
+        }
+        
+        if(value is not IEnumerable<KeyValuePair<string, string>> keyValuePairs)
+        {
+            throw new InvalidOperationException("The value must be an IEnumerable<KeyValuePair<string, string>>");
+        }
+
+        var invalidValues = keyValuePairs
+            .Where(x => !IsValid(x.Value))
+            .Select(x => $"{validationContext.MemberName}.{x.Value}")
+            .ToArray();
+
+        if (invalidValues.Length > 0)
+        {
+            result = new ValidationResult(FormatErrorMessage(validationContext.DisplayName), invalidValues);
+        }
+
+        return result;
+    }
+    
+    public override string FormatErrorMessage(string value)
+    {
+        return base.FormatErrorMessage("values of " + value);
+    }
+}
