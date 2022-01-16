671bad81f5101c25bf257f797f05d71032f98ae8
Kevin Smith <kev_bite@msn.com>
2022-01-16 10:41:31 +0000

    Adding ContainsAnyAttribute

---
+++ src/Exizent.ComponentModel.DataAnnotations/ContainsAnyAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Exizent.ComponentModel.DataAnnotations;
+
+public class ContainsAnyAttribute : ValidationAttribute
+{
+    private readonly Type _valueProviderType;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="valueProviderType">A type that implements IEnumerable which returns the valid values.</param>
+    public ContainsAnyAttribute(Type valueProviderType)
+    {
+        _valueProviderType = valueProviderType;
+    }
+
+    public override bool IsValid(object? value)
+    {
+        EnsureCorrectValueProviderType();
+        
+        if (value == null)
+            return true;
+
+        var values = (IEnumerable)Activator.CreateInstance(_valueProviderType);
+
+        return values.OfType<object>()
+            .Any(x => Equals(x, value));
+    }
+    
+    private void EnsureCorrectValueProviderType()
+    {
+        if (!typeof(IEnumerable).IsAssignableFrom(_valueProviderType))
+        {
+            throw new InvalidOperationException($"{nameof(ContainsAnyAttribute)} requires a type that implements IEnumerable");
+        }
+    }
+}
---
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/ContainsAnyAttributeTests.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+
+namespace Exizent.ComponentModel.DataAnnotations.Tests;
+
+public class ContainsAnyAttributeTests
+{
+    public class StringContainsAnyAttributeTests
+    {
+        class TestModel : IEnumerable
+        {
+            [ContainsAny(typeof(TestModel))] public string? CountryCode { get; set; }
+
+            public IEnumerator GetEnumerator()
+            {
+                IEnumerable<string> GetCountries()
+                {
+                    yield return "ES";
+                    yield return "GB";
+                    yield return "MX";
+                }
+
+                return GetCountries().GetEnumerator();
+            }
+        }
+
+        [Theory]
+        [InlineData("ES")]
+        [InlineData("GB")]
+        [InlineData("MX")]
+        public void ShouldBeValidWhenValueIsContainedInIEnumerableType(string countryCode)
+        {
+            var model = new TestModel { CountryCode = countryCode };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeTrue();
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ShouldBeValidForNullValue()
+        {
+            var model = new TestModel();
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeTrue();
+            results.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData("Invalid")]
+        [InlineData("Value")]
+        [InlineData("")]
+        public void ShouldBeInvalidWhenValueIsNotContainedInIEnumerableType(string countryCode)
+        {
+            var model = new TestModel { CountryCode = countryCode };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeFalse();
+            results[0].ErrorMessage.Should().Be($"The field {nameof(TestModel.CountryCode)} is invalid.");
+            results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestModel.CountryCode));
+        }
+    }
+
+    public class IntContainsAnyAttributeTests
+    {
+        class TestModel : IEnumerable
+        {
+            [ContainsAny(typeof(TestModel))] public int? PrimeNumber { get; set; }
+
+            public IEnumerator GetEnumerator()
+            {
+                IEnumerable<int> GetPrimeNumber()
+                {
+                    yield return 2;
+                    yield return 3;
+                    yield return 5;
+                    yield return 7;
+                    // etc...
+                }
+
+                return GetPrimeNumber().GetEnumerator();
+            }
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void ShouldBeValidWhenValueIsContainedInIEnumerableType(int primeNumber)
+        {
+            var model = new TestModel { PrimeNumber = primeNumber };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeTrue();
+            results.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ShouldBeValidForNullValue()
+        {
+            var model = new TestModel();
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeTrue();
+            results.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void ShouldBeInvalidWhenValueIsNotContainedInIEnumerableType(int primeNumber)
+        {
+            var model = new TestModel { PrimeNumber = primeNumber };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(model, context, results, true);
+
+            using var _ = new AssertionScope();
+            isValid.Should().BeFalse();
+            results[0].ErrorMessage.Should().Be($"The field {nameof(TestModel.PrimeNumber)} is invalid.");
+            results[0].MemberNames.Should().OnlyContain(x => x == nameof(TestModel.PrimeNumber));
+        }
+    }
+
+    public class InvalidContainsAnyAttributeTests
+    {
+        class TestModel
+        {
+            [ContainsAny(typeof(TestModel))]
+            public object? PrimeNumber { get; set; }
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenTypeDoesNotImplementIEnumerable()
+        {
+            var model = new TestModel();
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
+
+            Action action = () => Validator.TryValidateObject(model, context, results, true);
+
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("ContainsAnyAttribute requires a type that implements IEnumerable");
+        }
+    }
+}
