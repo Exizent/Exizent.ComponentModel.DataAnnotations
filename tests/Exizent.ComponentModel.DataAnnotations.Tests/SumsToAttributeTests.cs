5db0efa14ff4a4296baa45299a511c83637b1f30
Lee Blundell <lee.blundell@exizent.com>
2022-03-18 14:31:07 +0000

    Tidy up formatting

--- tests/Exizent.ComponentModel.DataAnnotations.Tests/SumsToAttributeTests.cs
+++ tests/Exizent.ComponentModel.DataAnnotations.Tests/SumsToAttributeTests.cs
@@ -101,68 +101,68 @@ public class SumsToAttributeTests
     public class WhenChildValueIsNullable
     {
         class NullableTestModel
-    {
-        public class Value
         {
-            public decimal? Percentage { get; set; }
+            public class Value
+            {
+                public decimal? Percentage { get; set; }
+            }
+
+            [SumsTo(nameof(Value.Percentage), 1)] public IEnumerable<Value>? Values { get; set; }
         }
 
-        [SumsTo(nameof(Value.Percentage), 1)] public IEnumerable<Value>? Values { get; set; }
-    }
-
-    [Fact]
-    public void ShouldBeValidWhenSumsToExpected()
-    {
-        var model = new NullableTestModel
+        [Fact]
+        public void ShouldBeValidWhenSumsToExpected()
         {
-            Values = new[]
+            var model = new NullableTestModel
             {
-                new NullableTestModel.Value
+                Values = new[]
                 {
-                    Percentage = 0.5m
-                },
-                new NullableTestModel.Value
-                {
-                    Percentage = 0.5m
+                    new NullableTestModel.Value
+                    {
+                        Percentage = 0.5m
+                    },
+                    new NullableTestModel.Value
+                    {
+                        Percentage = 0.5m
+                    }
                 }
-            }
-        };
-        var context = new ValidationContext(model);
-        var results = new List<ValidationResult>();
+            };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
 
-        var isValid = Validator.TryValidateObject(model, context, results, true);
+            var isValid = Validator.TryValidateObject(model, context, results, true);
 
-        using var _ = new AssertionScope();
-        isValid.Should().BeTrue();
-        results.Should().BeEmpty();
-    }
+            using var _ = new AssertionScope();
+            isValid.Should().BeTrue();
+            results.Should().BeEmpty();
+        }
 
-    [Fact]
-    public void ShouldBeInvalidWhenDoesNotSumAsExpected()
-    {
-        var model = new NullableTestModel
+        [Fact]
+        public void ShouldBeInvalidWhenDoesNotSumAsExpected()
         {
-            Values = new[]
+            var model = new NullableTestModel
             {
-                new NullableTestModel.Value
+                Values = new[]
                 {
-                    Percentage = null
-                },
-                new NullableTestModel.Value
-                {
-                    Percentage = 0.999m
+                    new NullableTestModel.Value
+                    {
+                        Percentage = null
+                    },
+                    new NullableTestModel.Value
+                    {
+                        Percentage = 0.999m
+                    }
                 }
-            }
-        };
-        var context = new ValidationContext(model);
-        var results = new List<ValidationResult>();
+            };
+            var context = new ValidationContext(model);
+            var results = new List<ValidationResult>();
 
-        var isValid = Validator.TryValidateObject(model, context, results, true);
+            var isValid = Validator.TryValidateObject(model, context, results, true);
 
-        using var _ = new AssertionScope();
-        isValid.Should().BeFalse();
-        results.Should().NotBeEmpty();
-        results.Should().OnlyContain(x => x.ErrorMessage == "The 'Percentage' members of 'Values' must sum to 1");
-    }
+            using var _ = new AssertionScope();
+            isValid.Should().BeFalse();
+            results.Should().NotBeEmpty();
+            results.Should().OnlyContain(x => x.ErrorMessage == "The 'Percentage' members of 'Values' must sum to 1");
+        }
     }
 }
